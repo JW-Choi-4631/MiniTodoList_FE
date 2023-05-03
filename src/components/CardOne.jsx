91f1ad459378dd4cd7ed5e04decb770be4c1a689
JW-Choi-4631 <choijung4631@naver.com>
2023-05-03 14:41:09 +0900

    function/common.js 파일 export 방식 변경 - 나중에 추가될 함수를 위해 여러개 적용할 수 있도록 변경

--- src/components/CardOne.jsx
+++ src/components/CardOne.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-import hiddenStyle from '../function/common'
+import { hiddenStyle } from '../function/common'
 
 const CardBox = styled.div`
   display : flex;
