461512c8826598c185bc89635c9d4f0e40ee50c8
JW-Choi-4631 <choijung4631@naver.com>
2023-05-12 20:49:32 +0900

    Action Creator 각 Component 내부로 이동

--- src/redux/modules/Card.js
+++ src/redux/modules/Card.js
@@ -1,39 +1,76 @@
-import { SAVE, COMPLETE, CHANGE, ERASE } from "./ActionCreator";
+const SAVE = "card/SAVE";
+const ERASE = "card/ERASE";
+const CHANGE = "card/CHANGE";
+const COMPLETE = "card/COMPLETE";
+
+export const save = (payLoad) => {
+  return {
+    type: SAVE,
+    payLoad,
+  };
+};
+
+export const change = (payLoad) => {
+  return {
+    type: CHANGE,
+    payLoad,
+  };
+};
+
+export const erase = (payLoad) => {
+  return {
+    type: ERASE,
+    payLoad,
+  };
+};
+
+export const complete = (payLoad) => {
+  return {
+    type: COMPLETE,
+    payLoad,
+  };
+};
 
 const initialState = [];
 
 const Card = (state = initialState, action) => {
+  const createDate = Date.now();
+  const random = Math.random();
 
-    const createDate = Date.now();
-    const random = Math.random();
-
-    switch (action.type) {
-        case SAVE:
-            // //향후 실시간 남은 시간 계산 시 사용
-            // let diff = new Date(action.payLoad.date) - createDate;
-            // let diffDay = Math.floor(diff / (1000*60*60*24));
-            // let diffHour = Math.floor((diff / (1000*60*60)) % 24);
-            // let diffMin = Math.floor((diff / (1000*60)) % 60);
-            // let diffSec = Math.floor(diff / 1000 % 60);
-            return [...state, {
-                id: (createDate + random).toString(),
-                title: action.payLoad.title,
-                context: action.payLoad.context,
-                date: action.payLoad.date,//`${diffDay}일 ${diffHour}시 ${diffMin}분 ${diffSec}초`
-                isDone: false,
-            }];
-        case ERASE:
-            const newCardList = state.filter(card => card.id !== action.payLoad);
-            return newCardList;
-        case COMPLETE:
-            const completeCardList = state.map((card) => (card.id === action.payLoad)? { ...card, isDone: !card.isDone }: card)
-            return completeCardList;
-        case CHANGE:
-            const changeCardList = state.map(card => (card.id === action.payLoad.id)? action.payLoad : card)
-            return changeCardList
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case SAVE:
+      // //향후 실시간 남은 시간 계산 시 사용
+      // let diff = new Date(action.payLoad.date) - createDate;
+      // let diffDay = Math.floor(diff / (1000*60*60*24));
+      // let diffHour = Math.floor((diff / (1000*60*60)) % 24);
+      // let diffMin = Math.floor((diff / (1000*60)) % 60);
+      // let diffSec = Math.floor(diff / 1000 % 60);
+      return [
+        ...state,
+        {
+          id: (createDate + random).toString(),
+          title: action.payLoad.title,
+          context: action.payLoad.context,
+          date: action.payLoad.date, //`${diffDay}일 ${diffHour}시 ${diffMin}분 ${diffSec}초`
+          isDone: false,
+        },
+      ];
+    case ERASE:
+      const newCardList = state.filter((card) => card.id !== action.payLoad);
+      return newCardList;
+    case COMPLETE:
+      const completeCardList = state.map((card) =>
+        card.id === action.payLoad ? { ...card, isDone: !card.isDone } : card
+      );
+      return completeCardList;
+    case CHANGE:
+      const changeCardList = state.map((card) =>
+        card.id === action.payLoad.id ? action.payLoad : card
+      );
+      return changeCardList;
+    default:
+      return state;
+  }
+};
 
 export default Card;
--- src/redux/modules/Content.js
+++ src/redux/modules/Content.js
@@ -1,33 +1,63 @@
-import { CLEAR, CONTEXT, DATE, TITLE } from "./ActionCreator";
+const TITLE = "content/TITLE";
+const CONTEXT = "content/CONTEXT";
+const DATE = "content/DATE";
+const CLEAR = "content/CLEAR";
+
+export const title = (payLoad) => {
+  return {
+    type: TITLE,
+    payLoad,
+  };
+};
+
+export const context = (payLoad) => {
+  return {
+    type: CONTEXT,
+    payLoad,
+  };
+};
+
+export const date = (payLoad) => {
+  return {
+    type: DATE,
+    payLoad,
+  };
+};
+
+export const clear = () => {
+  return {
+    type: CLEAR,
+  };
+};
 
 const initialState = {
-    title: '',
-    context: '',
-    date: '',
-}
+  title: "",
+  context: "",
+  date: "",
+};
 
 const SaveContent = (state = initialState, action) => {
-    switch (action.type) {
-        case TITLE:
-            return {
-                ...state,
-                title: action.payLoad
-            };
-        case CONTEXT:
-            return {
-                ...state,
-                context: action.payLoad,
-            };
-        case DATE:
-            return {
-                ...state,
-                date: action.payLoad
-            };
-        case CLEAR:
-            return initialState;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case TITLE:
+      return {
+        ...state,
+        title: action.payLoad,
+      };
+    case CONTEXT:
+      return {
+        ...state,
+        context: action.payLoad,
+      };
+    case DATE:
+      return {
+        ...state,
+        date: action.payLoad,
+      };
+    case CLEAR:
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 export default SaveContent;
